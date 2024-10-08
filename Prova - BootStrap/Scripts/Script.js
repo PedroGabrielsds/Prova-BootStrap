b624df22aa41be5a087a778b56afe222db5d644d
Mack <pedrogabrielsilva.d.s@gmail.com>
2024-10-08 16:56:16 -0300

    Prova - BootStrap

--- Prova - BootStrap/Scripts/Script.js
+++ Prova - BootStrap/Scripts/Script.js
@@ -1,10 +1,11 @@
 class Publicacoes{
-    constructor(Img, Titulo, Subtitulo, Descricao, Dono){
+    constructor(Img, Titulo, Subtitulo, Descricao, Dono, ImgDono){
         this.Img = Img;
         this.Titulo = Titulo;
         this.Subtitulo = Subtitulo;
         this.Descricao = Descricao;
         this.Dono = Dono;
+        this.ImgDono = ImgDono
     }
 
 }
@@ -15,8 +16,18 @@ TodasPublis.push(new Publicacoes("../images/cortado/natureza1.png", "Titulo", "S
 TodasPublis.forEach (CadaPubli => {
     document.querySelector("#Publis").innerHTML += `
         <div class="Postagens"> 
-            
+            <div id="Imagen">
+                <img src="${this.Img}">
+            </div>
+            <div id="Dono" class="Elements">
+                <h1>${this.Titulo}</h1>
+                <h2>${this.Subtitulo}</h2>
+                <p>${this.Descricao}</p>
+                <div id="Autor">
+                    <img src="${this.ImgDono}" alt="Foto">
+                    <p>Post de ${this.Dono}</p>
+                </div>
+            </div>
         </div>
-    
     `
 });
