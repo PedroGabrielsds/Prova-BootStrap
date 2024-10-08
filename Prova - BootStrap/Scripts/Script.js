9eb7fd928ae05453b3a224ee1bd2a222bf1adc23
Mack <pedrogabrielsilva.d.s@gmail.com>
2024-10-08 17:28:51 -0300

    Prova - BootStrap

--- Prova - BootStrap/Scripts/Script.js
+++ Prova - BootStrap/Scripts/Script.js
@@ -11,21 +11,22 @@ class Publicacoes{
 }
 
 var TodasPublis = []
-TodasPublis.push(new Publicacoes("../images/cortado/natureza1.png", "Titulo", "Subtitulo", "Descriçao", "Post de Sergio"))
+TodasPublis.push(new Publicacoes("/Prova - BootStrap/images/cortado/natureza1.png", "França", "Francês", "França fala fracês", "Sergio", "/Prova - BootStrap/images/original/time/sergio.jpg"))
 
-TodasPublis.forEach (CadaPubli => {
+
+TodasPublis.forEach(CadaPubli => {
     document.querySelector("#Publis").innerHTML += `
-        <div class="Postagens"> 
+        <div id="Posts"> 
             <div id="Imagen">
-                <img src="${this.Img}">
+                <img src="${CadaPubli.Img}" alt="Blabla">
             </div>
-            <div id="Dono" class="Elements">
-                <h1>${this.Titulo}</h1>
-                <h2>${this.Subtitulo}</h2>
-                <p>${this.Descricao}</p>
+            <div id="Dono">
+                <h1>${CadaPubli.Titulo}</h1>
+                <h2>${CadaPubli.Subtitulo}</h2>
+                <p>${CadaPubli.Descricao}</p>
                 <div id="Autor">
-                    <img src="${this.ImgDono}" alt="Foto">
-                    <p>Post de ${this.Dono}</p>
+                    <img src="${CadaPubli.ImgDono}" alt="Foto">
+                    <p>Post de ${CadaPubli.Dono}</p>
                 </div>
             </div>
         </div>
